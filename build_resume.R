cbd24a66ad7b758ed08c07c06a4788ad7f44b777
delimited0 <patrickding00@gmail.com>
2021-01-05 23:16:36 -0600

    jan 5 resume

--- build_resume.R
+++ build_resume.R
@@ -3,3 +3,7 @@ knitr::knit2pdf("resume/resume.Rmd", "resume/resume.pdf",
                 bib_engine = "bibtex")
 
 ?knitr::knit2pdf
+
+rmarkdown::render("resume/resume.Rmd", output_file = "resume/resume.pdf")
+
+knitr::knit()
